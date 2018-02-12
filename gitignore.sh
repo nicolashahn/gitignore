053b1c80bc5cda3e2d43daed32b733687338bd90
Nicolas Hahn <nicolas@stonespring.org>
2018-02-12 00:06:54 -0800

    bash best practices

--- gitignore.sh
+++ gitignore.sh
@@ -6,22 +6,23 @@ readlinkf() {
 }
 
 gitignore() {
-    if [ $# -eq 0 ]; then 
+    if [[ $# -eq 0 ]]; then 
         echo "usage: gitignore <path(s) to be added to .gitignore>"
+        exit 1
     fi
-    rootdir="`git rev-parse --show-toplevel`/"
+    rootdir="$(git rev-parse --show-toplevel)/"
     ignorefile="$rootdir.gitignore"
     for arg; do 
-        abs=`readlinkf $arg`
+        abs=$(readlinkf "$arg")
         rel=${abs/$rootdir/}
-        if [ -f "$abs" ] || [ -d "$abs" ]; then
-            touch $ignorefile
-            [[ $(tail -c1 $ignorefile) ]] && echo '' >> $ignorefile
-            grep -q -- "^$rel$" "$ignorefile" || echo "$rel" >> $ignorefile
+        if [[ -f "$abs" ]] || [[ -d "$abs" ]]; then
+            touch "$ignorefile"
+            [[ $(tail -c1 "$ignorefile") ]] && echo "" >> "$ignorefile"
+            grep -q -- "^$rel$" "$ignorefile" || echo "$rel" >> "$ignorefile"
         else
             echo "path '$rel' does not exist, skipping"
         fi
     done
 }
 
-gitignore $@
+gitignore "$@"
